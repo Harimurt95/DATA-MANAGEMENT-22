d7da7e02779385ad3ebb205523ffaa10ab67b54e
Shirui Gu <shirui.gu@warwick.ac.uk>
2024-03-19 16:27:37 +0000

    Add more plots in the Data_analysis.R

--- R/Data_analysis.R
+++ R/Data_analysis.R
@@ -40,6 +40,51 @@ ggplot(merge_data, aes(x = waiting_days, y = avg_review_rating)) +
 this_filename_date <- as.character(Sys.Date())
 # format the Sys.time() to show only hours and minutes 
 this_filename_time <- as.character(format(Sys.time(), format = "%H_%M"))
-ggsave(paste0("Figures/analysis_plot_",
+ggsave(paste0("Figures/ReveiwRating_plot_",
               this_filename_date,"_",
               this_filename_time,".png"))
+
+product_info <- PRODUCT %>%
+  mutate(category_group = case_when(
+    PRODUCT_CATEGORY_ID >= 11 & PRODUCT_CATEGORY_ID <= 15 ~ "Clothes",
+    PRODUCT_CATEGORY_ID >= 21 & PRODUCT_CATEGORY_ID <= 29 ~ "Electronics",
+    PRODUCT_CATEGORY_ID >= 31 & PRODUCT_CATEGORY_ID <= 39 ~ "Kitchenware",
+    TRUE ~ as.character(PRODUCT_CATEGORY_ID) 
+  ))
+
+product_counts <- product_info %>%
+  group_by(category_group) %>%
+  summarise(count = n())
+
+ggplot(product_counts, aes(x = category_group, y = count)) +
+  geom_bar(stat = "identity") +
+  theme_minimal() +
+  labs(title = "Number of Products in Each Parent Category",
+       x = "Category Group",
+       y = "Number of Products")
+
+ggsave(paste0("Figures/Category_plot_",
+              this_filename_date,"_",
+              this_filename_time,".png"))
+
+payment_info <- ORDER_DETAIL %>%
+  group_by(PAYMENT_METHOD_ID) %>%
+  summarise(Frequency = n()) %>%
+  mutate(Payment_Method = case_when(
+    PAYMENT_METHOD_ID == 1 ~ "Credit Card",
+    PAYMENT_METHOD_ID == 2 ~ "Debit Card",
+    PAYMENT_METHOD_ID == 3 ~ "Cash on Delivery",
+    TRUE ~ as.character(PAYMENT_METHOD_ID)))
+
+ggplot(payment_info, aes(x=Payment_Method,y=Frequency)) +
+  geom_bar(stat = "identity",width = 0.5) +
+  theme_minimal() +
+  labs(title = "Orders paid via different payment methods",
+       x = "Payment method",
+       y = "Number of Orders")
+
+ggsave(paste0("Figures/payment_plot_",
+              this_filename_date,"_",
+              this_filename_time,".png"))
+
+
